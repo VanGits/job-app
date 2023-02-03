18663e013e41f90a198505c2d1feb9382a8bb180
Van Celdran <celdranvan@gmail.com>
2023-02-02 23:51:03 -0800

    added update to nav for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Nav.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="Nav">
-      <div className="nav-content">
+      <div className={menuOpen ? "nav-content" : "nav-content open"}>
+        <div className="nav-hamburger">
+          <label htmlFor="check">
+            <input
+              type="checkbox"
+              id="check"
+              checked={menuOpen}
+              onChange={handleHamburgerClick}
+            />
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
         <h1 className="logo">LOGO</h1>
+
         <div className="nav-content-2">
           <h4>View Openings</h4>
           <h4>About</h4>
@@ -14,7 +35,9 @@ const Nav = () => {
           <h4>Contact</h4>
         </div>
         <div className="nav-content-buttons">
-          <Link to ="/login"><button className="button log-in">Login</button></Link>
+          <Link to="/login">
+            <button className="button log-in">Login</button>
+          </Link>
           <button className="button sign-up">Start Now</button>
         </div>
       </div>
